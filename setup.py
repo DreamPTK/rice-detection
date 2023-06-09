ff17879c41d3c3ca67c8be3e8949aa99449ade06
DPTK <107612381+Pathakorn40@users.noreply.github.com>
2023-06-09 15:18:42 +0700

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 from setuptools import setup, find_packages
 
-DESC ='This is a library that contain rice cultivated area detection tools and other geospatial tools for jupyter environment on https://sphere.gistda.or.th/ in part of Data Cube. Thus, you needs go to [sphere.gistda](https://sphere.gistda.or.th/) and [register the account](https://auth.sphere.gistda.or.th/auth/realms/sphere/protocol/openid-connect/registrations?client_id=frontend-iframe&redirect_uri=https%3A%2F%2Fsphere.gistda.or.th%2Fdashboard&state=b1580907-8f21-4a28-bd78-8cfa2ff9064b&response_mode=fragment&response_type=code&scope=openid&nonce=20300185-bc7b-4695-92c4-aa3159db0812&ui_locales=en) to access jupyter lab environment interactively from a browser.'
+DESC ='This is a library that contain rice cultivated area detection tools and other geospatial tools for jupyter environment on https://sphere.gistda.or.th/ in part of Data Cube. Thus, you needs go to sphere.gistda (https://sphere.gistda.or.th/) and register the account to access jupyter lab environment interactively from a browser.'
 
 setup(
     name='dream_river',
     packages=['dream_river'],
-    version='0.0.1',
+    version='0.0.2',
     license='MIT',
-    author='Dreamspareke',
     author_email='dreamusaha@gmail.com',
-    description= 'This is a library that contain rice detection tools and other geospatial tools for jupyter environment on https://sphere.gistda.or.th/ in part of Data Cube',
+    description= 'This is a library contained rice detection tools and other geospatial tools for jupyter environment on sphere.gistda.or.th in part of Data Cube',
     long_description= DESC,
     author='Pathakorn Usaha',
     url= 'https://github.com/Pathakorn40/rice-detection',
